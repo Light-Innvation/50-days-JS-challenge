9b68789317361d8f030ff55d002c0ba38e3ecc21
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-03-10 18:39:51 +0100

    multiplication table of each number from 1 to 10

---
+++ day9.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 11; i++) {
+    console.log(`Multiplication number for ${i}:`);
+    for (let j = 0; j < 11; j++) {
+        
+        console.log(`Multiplicatio of ${i} * ${ j} = ${i * j}`)
+    }
+    console.log("/n")
+    
+}
