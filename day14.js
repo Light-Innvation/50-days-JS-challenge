a72855ae0e389f288c3e82efeb1b841daedd7e40
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-03-13 10:36:29 +0100

    calculate the fibonnaci of a number without using the recursion mode

---
+++ day14.js
@@ -0,0 +1,10 @@
+function firstFibonnaci(n){
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[ i - 2];
+        
+    }
+    return fib.slice( 0,n);
+
+}
+console.log(firstFibonnaci(10));
