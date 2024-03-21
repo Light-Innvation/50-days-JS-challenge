38c40ef9d3275735fef9d1c4400f0688b93b04d4
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-03-21 11:43:09 +0100

    added another method to solve #20

--- day20.js
+++ day20.js
@@ -10,3 +10,17 @@
 const array = [1,2,3,4,5,5,6];
 console.log("original Array", array);
 console.log( "Array rotated right by 1 position:", rotateRight(array));
+
+//anothermethod which can be use
+function rotateRight(arr){
+    if (arr.length <= 1) {
+        return arr;
+    }
+    const LastElement2 = arr.slice(-1);
+    const rotatedArray = LastElement2.concat(arr.slice(0, -1));
+    return rotatedArray;
+    
+}
+const array2 = [1,2,3,4,5,5,6];
+console.log(" Array", array2);
+console.log( "Array rotated right by 1 position:", rotateRight(array2));
